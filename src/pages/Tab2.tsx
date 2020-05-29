51fe7a34c48639a57657d36f33e2fe5fbb2039a2
Michael Sun <michael.mu.sun@gmail.com>
2020-05-29 15:08:49 -0700

    renamed project

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,23 +1,115 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import React, { useState } from "react";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+  IonText,
+} from "@ionic/react";
+import { camera, trash, close } from "ionicons/icons";
+import { usePhotoGallery, Photo } from "../hooks/usePhotoGallery";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 
 const Tab2: React.FC = () => {
+  const [barcode, setBarcode] = useState("-1");
+  const { deletePhoto, photos, takePhoto } = usePhotoGallery(barcode);
+  const [photoToDelete, setPhotoToDelete] = useState<Photo>();
+
+  // https://stackoverflow.com/questions/50009585/error-error-uncaught-in-promise-cordova-not-available-with-ionic-serve
+  // I know that cordova plugins are not available with ionic serve
+  const openScanner = async () => {
+    const scannedBC = await BarcodeScanner.scan();
+    setBarcode(scannedBC.text);
+    console.log(`Barcode data: ${scannedBC.text}`);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Photo Gallery</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonRow>
+          <IonCol>
+            <IonText>{barcode}</IonText>
+          </IonCol>
+          <IonCol>
+            <IonButton onClick={openScanner}>Scan Barcode</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonGrid>
+          <IonRow>
+            {/* Location: mtp:/HD1905/Internal shared storage/Android/data/io.ionic.starter/files/Pictures/ */}
+            {/* filter out photos that match barcode, then map those to display in the grid on screen */}
+            {photos.filter((photo, index) => barcode == photo.filepath.split("/").slice(-1)[0].split("_")[0])
+            .map((photo, index) => (
+                <IonCol size="6" key={index}>
+                  <IonImg
+                    onClick={() => setPhotoToDelete(photo)}
+                    src={photo.base64 ?? photo.webviewPath}
+                  />
+                  {photo.filepath}
+                </IonCol>            
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        {/* Displays list of photo filepaths for debugging*/}
+        {/* <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+                <IonCol size="6" key={index}>
+                  {photo.filepath}
+                </IonCol>            
+            ))}
+          </IonRow>
+        </IonGrid> */}
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[
+            {
+              text: "Delete",
+              role: "destructive",
+              icon: trash,
+              handler: () => {
+                if (photoToDelete) {
+                  deletePhoto(photoToDelete);
+                  setPhotoToDelete(undefined);
+                }
+              },
+            },
+            {
+              text: "Cancel",
+              icon: close,
+              role: "cancel",
+            },
+          ]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
       </IonContent>
     </IonPage>
   );
